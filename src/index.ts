20dc1074891eb716b6767a8132fd98d9dd6b988f
Septs <github@septs.pw>
2021-01-05 05:06:58 +0800

    Regular update: 2021-01-04T21:06:58Z

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ import http from 'http';
 import https from 'https';
 import makeHandler from 'lambda-request-handler';
 import Telegraf from 'telegraf';
-import { autoReply, errorLog } from './middleware';
+import {
+  autoPoll,
+  autoReply,
+  deleteBotCommandMessage,
+  deleteMessage,
+  errorLog,
+  kickChatMember,
+  tap,
+  unpinChatMessage,
+} from './middleware';
 import taokouling from './taokouling';
 
 const bot = new Telegraf(process.env.BOT_TOKEN!, {
@@ -15,64 +24,13 @@ const bot = new Telegraf(process.env.BOT_TOKEN!, {
 });
 
 bot.use(Telegraf.log());
-
 bot.use(errorLog);
-
-bot.use(taokouling);
-
+bot.use(tap(taokouling));
+bot.use(unpinChatMessage);
+bot.use(deleteBotCommandMessage);
 bot.hears(/(讨论|[加入主])群/, autoReply);
-
-bot.on('message', async (ctx, next) => {
-  const onDelete = async () => {
-    const isDeleteMessage = !(
-      ctx.message?.from?.id === 777000 ||
-      ctx.message?.reply_to_message ||
-      ctx.message?.new_chat_members
-    );
-    if (isDeleteMessage) {
-      return ctx.deleteMessage();
-    }
-  };
-  const onPoll = async () => {
-    if (ctx.message?.from?.id !== 777000) {
-      return;
-    }
-    const title = '🗳️';
-    const options = ['👍', '👎', '🎉', '😕', '👀', '💊'];
-    const poll = await ctx.replyWithPoll(title, options, {
-      reply_to_message_id: ctx.message!.message_id,
-      disable_notification: true,
-    });
-    console.info('Poll ID', poll.poll.id);
-  };
-  return Promise.all([onDelete(), onPoll(), next()]);
-});
-
-bot.on('new_chat_members', autoReply, async (ctx) => {
-  for (const { id, is_bot } of ctx.message!.new_chat_members!) {
-    if (is_bot) {
-      continue;
-    }
-    try {
-      await ctx.kickChatMember(id, Date.now() / 1000 + 300);
-      await ctx.unbanChatMember(id);
-    } catch (err) {
-      // continue on error
-      console.error(err);
-    }
-  }
-});
-
-bot.on('pinned_message', (ctx) => ctx.unpinChatMessage());
-
-bot.on('text', (ctx) => {
-  const entry = ctx.message?.entities?.find(
-    ({ type }) => type === 'bot_command',
-  );
-  if (entry?.offset === 0) {
-    return ctx.deleteMessage();
-  }
-});
+bot.on('message', autoPoll, deleteMessage);
+bot.on('new_chat_members', autoReply, kickChatMember);
 
 export const handler = makeHandler(
   bot.webhookCallback(process.env.BOT_HOOK_PATH!),
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,6 @@
-import { Context, Middleware } from 'telegraf';
+import { Composer, Context, Middleware } from 'telegraf';
+import { MiddlewareFn } from 'telegraf/typings/composer';
+import { shuffle } from 'lodash-es';
 
 export const errorLog: Middleware<Context> = async (ctx, next) => {
   try {
@@ -8,26 +10,85 @@ export const errorLog: Middleware<Context> = async (ctx, next) => {
   }
 };
 
-export const autoReply: Middleware<Context> = async (ctx, next) => {
-  const reply = async () => {
-    const { chat, message_id } = await ctx.reply('Reading', {
-      reply_to_message_id: ctx.message!.message_id,
-    });
-    const editMessageText = (text: string) =>
-      ctx.telegram.editMessageText(chat.id, message_id, undefined, text, {
-        disable_web_page_preview: true,
-        parse_mode: 'HTML',
-      });
-    const { description } = await ctx.getChat();
-    try {
-      if (description) {
-        await editMessageText(description);
-      } else {
-        await ctx.deleteMessage(message_id);
-      }
-    } catch (error) {
-      await editMessageText(String(error));
-    }
-  };
-  return Promise.all([reply(), next()]);
+export const tap = (middleware: Middleware<Context>): MiddlewareFn<Context> => {
+  const fn = Composer.unwrap(middleware);
+  return (ctx, next) =>
+    Promise.all([fn(ctx, Promise.resolve.bind(Promise)), next()]);
 };
+
+export const autoReply = tap(async (ctx) => {
+  const { chat, message_id } = await ctx.reply('Reading', {
+    reply_to_message_id: ctx.message!.message_id,
+  });
+  const editMessageText = (text: string) =>
+    ctx.telegram.editMessageText(chat.id, message_id, undefined, text, {
+      disable_web_page_preview: true,
+      parse_mode: 'HTML',
+    });
+  const { description } = await ctx.getChat();
+  try {
+    if (description) {
+      await editMessageText(description);
+    } else {
+      await ctx.deleteMessage(message_id);
+    }
+  } catch (error) {
+    await editMessageText(String(error));
+  }
+});
+
+export const autoPoll = tap(async (ctx) => {
+  if (ctx.message?.from?.id !== 777000) {
+    return;
+  }
+  const title = '🗳️';
+  const options = shuffle(['👍', '👎', '🎉', '😕', '👀', '💊']);
+  const poll = await ctx.replyWithPoll(title, options, {
+    reply_to_message_id: ctx.message!.message_id,
+    disable_notification: true,
+  });
+  console.info('Poll ID', poll.poll.id);
+});
+
+export const kickChatMember = tap(async (ctx) => {
+  for (const { id, is_bot } of ctx.message!.new_chat_members!) {
+    if (is_bot) {
+      continue;
+    }
+    try {
+      await ctx.kickChatMember(id, Date.now() / 1000 + 300);
+      await ctx.unbanChatMember(id);
+    } catch (err) {
+      // continue on error
+      console.error(err);
+    }
+  }
+});
+
+export const deleteMessage = tap(async (ctx) => {
+  const isDeleteMessage = !(
+    ctx.message?.from?.id === 777000 ||
+    ctx.message?.reply_to_message ||
+    ctx.message?.new_chat_members
+  );
+  if (isDeleteMessage) {
+    return ctx.deleteMessage();
+  }
+});
+
+export const deleteBotCommandMessage = tap((ctx) => {
+  const entry = ctx.message?.entities?.find(
+    ({ type }) => type === 'bot_command',
+  );
+  if (entry?.offset === 0) {
+    return ctx.deleteMessage();
+  }
+});
+
+export const unpinChatMessage = tap((ctx) => {
+  if (ctx.message?.pinned_message) {
+    return ctx.unpinChatMessage();
+  } else if (ctx.message?.from?.id === 777000) {
+    return ctx.unpinChatMessage();
+  }
+});
